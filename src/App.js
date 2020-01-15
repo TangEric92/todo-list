bc202e32c867609cb781f499fa4389e18fe5502d
alex.dana@epitech.eu <alex.dana@epitech.eu>
2020-01-15 20:43:08 +0100

    first step

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleTitleChange = e => {
+    setTitle(e.target.value);
+  };
+
+  const submitTitle = e => {
+    e.preventDefault();
+    // On check que title ne soit pas vide
+    if (title === "") return;
+    // On a defini notre tache
+    const task = { title: title, state: false };
+    // On ajoute notre task a notre liste de tache
+    tasks.push(task);
+    // On update tasks avec le nouveau tableau
+    setTasks(tasks);
+    // On clean title
+    setTitle("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do list</h1>
+      {tasks &&
+        tasks.map((task, index) => {
+          return <p>{task.title}</p>;
+        })}
+      <form onSubmit={submitTitle}>
+        <input
+          placeholder="Titre"
+          type="text"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
