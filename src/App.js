78683c5be186a76a7882b422fe52fca151e87d7d
alex.dana@epitech.eu <alex.dana@epitech.eu>
2020-01-16 20:05:12 +0100

    To do list with bonus

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
 
+import Task from "./components/Task";
+
 function App() {
   const [title, setTitle] = useState("");
+  const [search, setSearch] = useState("");
   const [tasks, setTasks] = useState([]);
 
   const handleTitleChange = e => {
     setTitle(e.target.value);
+  };
+
+  const handleSearchChange = e => {
+    setSearch(e.target.value);
   };
 
   const submitTitle = e => {
@@ -22,12 +29,52 @@ function App() {
     setTitle("");
   };
 
+  const changeState = index => {
+    // On recupere l'élément a la position de l'index
+    // Et on change son etat
+    const task = tasks[index];
+    task.state = !task.state;
+    tasks.splice(index, 1);
+
+    if (task.state) {
+      // On créé un nouveau tableau pour mettre a jour la vue
+      setTasks([...tasks, task]);
+    } else {
+      setTasks([task, ...tasks]);
+    }
+  };
+
+  const deleteTask = index => {
+    // On supprime l'élément a la position de l'index
+    tasks.splice(index, 1);
+    // On créé un nouveau tableau pour mettre a jour la vue
+    setTasks([...tasks]);
+  };
+
   return (
     <div className="App">
       <h1>To-Do list</h1>
+      <input
+        placeholder="Search"
+        type="text"
+        value={search}
+        onChange={handleSearchChange}
+      />
       {tasks &&
         tasks.map((task, index) => {
-          return <p key={index}>{task.title}</p>;
+          if (search === "" || task.title.includes(search)) {
+            return (
+              <Task
+                key={`Task:${index}`}
+                index={index}
+                title={task.title}
+                state={task.state}
+                deleteTask={deleteTask}
+                changeState={changeState}
+              />
+            );
+          }
+          return null;
         })}
       <form onSubmit={submitTitle}>
         <input
---
+++ src/components/Task.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Task.css";
+
+function Task(props) {
+  const { index, title, state, deleteTask, changeState } = props;
+
+  return (
+    <div className="task-wrapper">
+      <span className="task-delete" onClick={() => deleteTask(index)}>
+        X
+      </span>
+      <span
+        className="task-title"
+        onClick={() => changeState(index)}
+        // Grace au props "style", qui prend un objet en parametre, on ajoute du style au dessus du style defini dans le className
+        // On fait une ternaire pour definir si le texte doit etre barré ou non
+        style={{ textDecoration: state ? "line-through" : "" }}
+      >
+        {title}
+      </span>
+    </div>
+  );
+}
+
+export default Task;
